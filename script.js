9c530c20e442d596452890aa869b4163a15d73e0
Kaviyaselvaraj05 <kaviyabio37@gmail.com>
2024-07-09 22:02:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementById("Foetus").addEventListener("click",function(){
+   document.getElementById("image").src="https://t4.ftcdn.net/jpg/05/88/19/37/360_F_588193739_S6uXdOJ32lr5L8x5bgzmgU845q0eSf1X.jpg"
+});
+document.getElementById("Infancy").addEventListener("click",function(){
+   document.getElementById("image").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj2ihMBniT_FZfSanatIjfR0goUeZ9Wwluwg&s"
+});
+
+document.getElementById("Toddler Years").addEventListener("click",function(){
+    document.getElementById("image").src="https://raisingchildren.net.au/__data/assets/image/0011/50042/toddlers-play-hero_narrow.jpg"
+});
+
+document.getElementById("Childhood").addEventListener("click",function(){
+    document.getElementById("image").src="https://www.itisaras.org/projectdhaara/wp-content/uploads/sites/2/2020/10/child-2769913_1280-1024x774.jpg"
+});
+
+document.getElementById("Adolescence").addEventListener("click",function(){
+    document.getElementById("image").src="https://www.hopkinsmedicine.org/-/media/images/health/3_-wellness/kids-and-teens-health/growing-child-adolescent-teaser.jpg"
+});
+
+document.getElementById("Adulthood").addEventListener("click",function(){
+    document.getElementById("image").src="https://www.praveensaanker.ae/wp-content/uploads/2023/11/Coaching-For-Early-Adulthood.png"
+});
+
+document.getElementById("Middle Age").addEventListener("click",function(){
+    document.getElementById("image").src="https://s3-us-west-2.amazonaws.com/courses-images/wp-content/uploads/sites/3707/2014/09/17043024/african-american-african-descent-american-asian-black-casual-1444755-pxhere.com_.jpg"
+});
+
+document.getElementById("Old Age").addEventListener("click",function(){
+    document.getElementById("image").src="https://tribecacare.com/wp-content/uploads/2019/02/fun-activities-in-old-age-home.png"
+});
